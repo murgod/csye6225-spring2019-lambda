5ae93a1e30e25f8069a54d77e05f00677ab5ef93
iampaavan <46580953+iampaavan@users.noreply.github.com>
2019-04-02 14:30:25 -0400

    .

--- Lambda/src/main/java/csye6225/lambda/LogEvent.java
+++ Lambda/src/main/java/csye6225/lambda/LogEvent.java
@@ -51,9 +51,7 @@ public class LogEvent implements RequestHandler<SNSEvent, Object> {
 			this.dynamo.getTable(TABLE_NAME).putItem(new PutItemSpec()
 					.withItem(new Item().withString("id", userName).withString("Token", token).withLong("TTL", 1200)));
 			this.body = "Password reset link here";
-		} else {
-			this.body = "Password reset link aready sent";
-		}
+		
 		try {
 			Content subject = new Content().withData(SUBJECT);
 			Content textbody = new Content().withData(body);
@@ -68,6 +66,7 @@ public class LogEvent implements RequestHandler<SNSEvent, Object> {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
 		return null;
 	}
 
