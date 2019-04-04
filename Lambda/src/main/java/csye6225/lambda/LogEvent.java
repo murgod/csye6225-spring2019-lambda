1147a48a4d121d29c6eea03ded584b99b01ad3d0
murgod <murgod.a@husky.neu.edu>
2019-04-03 17:52:20 -0700

    resolvig conflicts now

--- Lambda/src/main/java/csye6225/lambda/LogEvent.java
+++ Lambda/src/main/java/csye6225/lambda/LogEvent.java
@@ -51,7 +51,9 @@ public class LogEvent implements RequestHandler<SNSEvent, Object> {
 			this.dynamo.getTable(TABLE_NAME).putItem(new PutItemSpec()
 					.withItem(new Item().withString("id", userName).withString("Token", token).withLong("TTL", 1200)));
 			this.body = "Password reset link here";
-		
+		}else {
+                        this.body = "Password reset link aready sent";
+                }		
 		try {
 			Content subject = new Content().withData(SUBJECT);
 			Content textbody = new Content().withData(body);
